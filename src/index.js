ee7152360ad932acb245e64261658f3985a2d696
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-02 15:55:19 -0500

    Resolving heroku react-router-dom problem

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+
 import './index.css';
 import './general.css';
+
+
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
