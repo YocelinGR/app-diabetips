541b132c067374684b9b42464f4f7ac90ea020d0
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-10-02 14:31:09 -0500

    Adding the routes for the 3 first views and the auth for facebook, gmail, twitter and email with redirect option

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './LogIn.css';
 import {
- Button, Row, Col,
-  Input} from 'react-materialize';
+  Button, Row, Col,
+  Input,
+} from 'react-materialize';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+/*
 class LogIn extends Component {
   render() {
     return (
       <div>
-        <Row className= "Header-Login">
+        <Row className="Header-Login">
           <Col s={10}>
             <img src="https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-social-network/blob/master/src/images/LogoDiabeTips.png?raw=true" />
           </Col>
@@ -19,31 +23,74 @@ class LogIn extends Component {
           </Col>
         </Row>
         <Row>
-          <Col s={11} className ="notRegistryYet">
+          <Col s={11} className="notRegistryYet">
             <h5>Ingresa tus datos: </h5>
             <Input type="email" label="Email" s={10} />
             <Input type="password" label="Password" s={10} />
             <p s={10}>Nosotros nunca compartiremos tu información con nadie.</p>
             <Input s={10} name="group1" type="checkbox" value="remember" label="Recuerdame" />
-            <div className ="signBtn">
-              <Button id= "registrarBtn" waves="light">Registrar</Button>
+            <div className="signBtn">
+              <Button id="registrarBtn" waves="light">Registrar</Button>
             </div>
             <p s={10}>o también puedes ...</p>
           </Col>
         </Row>
         <Row>
-          <div className= "signBtn">
-          <Button waves="light" id ="btnGmail"><i className="fa fa-google">     Gmail</i></Button>
-          <Button waves="light" id= "btnFacebook"><i className="fa fa-facebook-official">     Facebook</i></Button>
-        </div>
-          <div className= "notRegistryYet">
-          <p s={10}>No estas registrado</p>
-          <p s={10}>Registrate aquí.</p>
-        </div>
+          <div className="signBtn">
+            <Button waves="light" id="btnGmail"><i className="fa fa-google">     Gmail</i></Button>
+            <Button waves="light" id="btnFacebook"><i className="fa fa-facebook-official">     Facebook</i></Button>
+          </div>
+          <div className="notRegistryYet">
+            <p s={10}>No estas registrado</p>
+            <p s={10}>Registrate aquí.</p>
+          </div>
         </Row>
       </div>
     );
   }
+} */
+firebase.initializeApp({
+  apiKey: 'AIzaSyBoO-dyO4PywqOJJVLDAucttJLMlM5N3vs',
+  authDomain: 'diabetipsoficial.firebaseapp.com'
+});
 
+class LogIn extends Component{
+  state = { isSignedIn: false}
+  uiConfig = {
+    signInFlow: "redirect",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      sigInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user})
+      console.log("user", user);
+    });
+  }
+
+  render() {
+    return(
+      <div className= "LogInStyle">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Hola, hoy serás una mejor versión de ti misma</div>
+            <button onClick={() => firebase.auth().signOut()}>Salir</button>
+            <h1>Diabeamigo: {firebase.auth().currentUser.displayName}</h1>
+            <img alt = "foto de usuario" src= {firebase.auth().currentUser.photoURL} />
+          </span>
+        ) : (
+          <StyledFirebaseAuth uiConfig= {this.uiConfig} firebaseAuth = {firebase.auth()} />
+        )}
+      </div>
+    )
+  }
 }
 export default LogIn;
