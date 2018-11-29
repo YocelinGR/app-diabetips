4fc777cb9bd1b7d79a9c3f16bada9feddb094273
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-29 13:22:07 -0600

    Adding the complete responsive view logup and and the navbar from wall

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,32 +2,33 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import { Button, Icon, Row, Col, Footer
-       } from 'react-materialize';
+import {
+ Button, Icon, Row, Col, Footer,
+} from 'react-materialize';
 
 class Home extends Component {
   render() {
     return (
-      <div className= "App">
+      <div className="App">
         <section className="container">
-            <Col s={10}>
-                <img src= "https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-social-network/blob/master/src/images/LogoDiabeTips.png?raw=true" />
+          <Col s={10}>
+              <img src="https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-social-network/blob/master/src/images/LogoDiabeTips.png?raw=true" />
+            </Col>
+        </section>
+        <div className="app-carousel">
+          <Col s={2}>
+                <p>Mejoremos juntas nuestra calidad de vida.</p>
               </Col>
-          </section>
-        <div className= "app-carousel">
-              <Col s={2}>
-              <p>Mejoremos juntas nuestra calidad de vida.</p>
-            </Col>
-              <Col s={10}>
-              <img src ="https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-social-network/blob/master/src/images/mexicanDiab.jpg?raw=true" />
-            </Col>
-            </div>
+          <Col s={10}>
+                <img src="https://github.com/YocelinGR/cdmx-2018-06-bc-core-am-social-network/blob/master/src/images/mexicanDiab.jpg?raw=true" />
+              </Col>
+        </div>
         <Footer className="App-footer">
-              <Col s={12}>
-                <a className= "btn-footer" id="unirse"><Link to='/LogUp'>UNIRSE</Link></a>
-                <a className ="btn-footer" id="iniciar"><Link to='/LogIn'>INICIAR</Link></a>
+          <Col s={12}>
+                <a className="btn-footer" id="unirse"><Link to="/LogUp">UNIRSE</Link></a>
+                <a className="btn-footer" id="iniciar"><Link to="/LogIn">INICIAR</Link></a>
               </Col>
-            </Footer>
+        </Footer>
       </div>
     );
   }
--- src/components/LogUp.js
+++ src/components/LogUp.js
@@ -9,25 +9,25 @@ class LogUp extends Component {
   render() {
     return (
       <div>
-        <Col s={10}>
-                  <h4 s={10}>Por favor, ingresa tus datos: </h4>
-                  <Input s={10} label="Nombre Completo" />
-                  <Input s={10} label="Nombre de Usuario" />
-                  <Input type="password" label="Password" s={10} />
-                  <Input type="email" label="Email" s={10} />
-                  <Input s={10} name="on" type="date" label="Cumpleaños" onChange={function (e, value) {}} />
-                  <Input s={10} type="select" label="País">
-                    <option value="1">Perú</option>
-                    <option value="2">México</option>
-                    <option value="3">Brasil</option>
-                    <option value="3">Chile</option>
-                  </Input>
-                  <Input s={10} name="group1" type="checkbox" value="remember" label="Recuerdame" />
-                  <p s={10}>Nosotros nunca compartiremos tu información con nadie.</p>
-                  <div className="signBtn">
-                    <Button id="registrarBtn" waves="light">Hecho!</Button>
-                  </div>
-                </Col>
+        <Col s={10} m={11} l={12}>
+          <h4 s={10} m={11} l={12}>Por favor, ingresa tus datos: </h4>
+          <Input s={10} m={11} l={12} label="Nombre Completo" />
+          <Input s={10} m={11} l={12} label="Nombre de Usuario" />
+          <Input type="password" label="Password" s={10} m={11} l={12} />
+          <Input type="email" label="Email" s={10} m={11} l={12} />
+          <Input s={10} m={11} l={12} name="on" type="date" label="Cumpleaños" onChange={function (e, value) {}} />
+          <Input s={10} m={11} l={12} type="select" label="País">
+            <option value="1">Perú</option>
+            <option value="2">México</option>
+            <option value="3">Brasil</option>
+            <option value="3">Chile</option>
+          </Input>
+          <Input s={10} m={11} l={12} name="group1" type="checkbox" value="remember" label="Recuerdame" />
+          <p s={10} m={11} l={12}>Nosotros nunca compartiremos tu información con nadie.</p>
+          <div className="signBtn">
+            <Button id="registrarBtn" waves="light">Hecho!</Button>
+          </div>
+        </Col>
       </div>
     );
   }
