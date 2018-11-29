a218bca2987dc3fd938facb247b2b8ea8f674351
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-29 14:32:20 -0600

    Adding Mis Post from navbar

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -4,7 +4,6 @@ import {NavLink} from 'react-router-dom';
 const SignedInLinks = () => {
     return(
         <ul className= "right">
-            <li><NavLink to ='/'>Mis Post</NavLink></li>
             <li><NavLink to ='/LogUp'>Mis Perfil</NavLink></li>
             <li><NavLink to ='/' className = "btn btn-floating pink lighten-1">Salir</NavLink></li>
         </ul>
