8c4526468b5f7b0c879973eae245f3aa11cb7903
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-11-29 04:58:59 -0600

    Areglando responsive en mobile pantalla de inicio

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
 
-const NavBar = () => {
-    return(
-        <nav className="nav-wrapper grey darken-3">
-        <div className = "container">
-            <Link to='/LogIn' className="brand-logo"></Link>
-            <SignedInLinks />
-            <SignedOutLinks />
+const NavBar = () => (
+  <nav className="nav-wrapper grey darken-3">
+  <div className="container">
+      <div className="row">
+          <div className="col-sm-12">
+              <Link to="/LogIn" className="brand-logo" />
+              <SignedInLinks />
+              <SignedOutLinks />
+            </div>
         </div>
-        </nav>
-    );
-}
+
+    </div>
+</nav>
+);
 export default NavBar;
